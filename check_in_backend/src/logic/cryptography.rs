2bcbc66e7d0348fa0f4925f1eb04ff7c6f1c0a60
apatheticfrog <apathetic_frog@protonmail.com>
2021-11-08 16:37:17 +0100

    swapped to postgres because relations

--- check_in_backend/src/logic/cryptography.rs
+++ check_in_backend/src/logic/cryptography.rs
@@ -1,6 +1,20 @@
 use argon2::{self, Config};
+use rand::Rng;
 
+lazy_static! {
+    static ref CONFIG: Config<'static> = Config::default();
+}
 
-pub fn hash_and_salt_password(password: String){
-    let salt = 
+pub fn generate_salt() -> [u8; 8] {
+    rand::thread_rng().gen::<[u8; 8]>()
+}
+
+pub fn hash_and_salt_password(password: String, salt: [u8; 8]) -> Result<String, argon2::Error> {
+    let password = password.as_bytes();
+    let encoded = argon2::hash_encoded(password, &salt, &CONFIG.to_owned());
+    encoded
+}
+
+pub fn verify_password(hash: String, password_bytes: [u8; 8]) -> Result<bool, argon2::Error> {
+    argon2::verify_encoded(&hash, &password_bytes)
 }
--- check_in_gui/src/main.py
+++ check_in_gui/src/main.py
@@ -1,6 +1,4 @@
 from interface.check_in_main import CheckInApp
 
-
-
 if __name__ == '__main__':
     CheckInApp().run()
